e02df52fff05ff241ba6954e6cdd6cafa476b65a
Ryrya Astrey <119737958+RyryaAstrey@users.noreply.github.com>
2023-01-06 03:47:16 +0000

    Rspecによるテスト開始

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
   def create
     @book=Book.new(book_params)
     if @book.save
-      redirect_to books_path(@book.id)
+      flash[:notice]="Book was successfully created."
+      redirect_to book_path(@book.id)
     else
       @books=Book.all
       render :index
@@ -28,16 +29,18 @@ class BooksController < ApplicationController
   def update
     @book= Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to '/books'
+      flash[:notice]="Book was successfully updated."
+      redirect_to book_path(@book.id)
     else
       render :edit
     end
   end
 
   def destroy
-    list = Book.find(params[:id])
-    list.destroy
-    redirect_to '/books'
+    book = Book.find(params[:id])
+    book.destroy
+    flash[:notice]="Book was successfully destroyed."
+    redirect_to books_path
   end
 
   private
